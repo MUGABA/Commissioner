f94585b6f84cb5c3940146683326c6b11fd7a0dc
MUGABA <mugabamuha@gmail.com>
2023-03-24 18:16:36 +0300

    making all the changes

--- src/startup.js
+++ src/startup.js
@@ -10,6 +10,10 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
+  app.get("/", (req, res) => {
+    res.status(200).send({ message: "message received" });
+  });
+
   app.use("/auth", Users);
   app.use("/food", Food);
   app.use("/cares", Care);
