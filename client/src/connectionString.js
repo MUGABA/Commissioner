7f903fbdcb611e518b2b76486e39e33910e382e9
MUGABA <mugabamuha@gmail.com>
2023-03-24 17:17:50 +0300

    all changes

--- client/src/connectionString.js
+++ client/src/connectionString.js
@@ -1,3 +1,3 @@
 export default {
-  url: 'postgres://app_rw_pvhlxqoh633d7ke5j6n5fikgcy:ARozWGD1lU-T-PgTi0S6k0AYRlxTqkag@pg-tunnel.borealis-data.com:55859/dkj6ffetoqkyxtdtp7epvv6fg2a',
+  url: 'https://uganda-nutrition-app.herokuapp.com',
 };
