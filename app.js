6b0d84f587394d52ee2c6d316f7910d4b1ce92aa
MUGABA <mugabamuha@gmail.com>
2023-03-24 20:25:59 +0300

    pushing static files

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, ".", "client/build")));
 
 require("./src/startup")(app);
 connection();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, ".", "client/build", "index.html"));
 });
 
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("Server is running now"));
