a99029fce66492213b60f3b5be1621b00564de9d
MUGABA <mugabamuha@gmail.com>
2023-03-24 17:00:59 +0300

    made changes well

--- client/src/connectionString.js
+++ client/src/connectionString.js
@@ -1,3 +1,3 @@
 export default {
-  url: 'http://localhost:5006',
+  url: 'postgres://app_rw_pvhlxqoh633d7ke5j6n5fikgcy:ARozWGD1lU-T-PgTi0S6k0AYRlxTqkag@pg-tunnel.borealis-data.com:55859/dkj6ffetoqkyxtdtp7epvv6fg2a',
 };
